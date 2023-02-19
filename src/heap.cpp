ee2abc501a5fc707486ec30143846e258e1f22a2
msula2 <msula2@uis.edu>
2023-02-19 07:03:55 +0500

    Heapify working, segmentation fault due to malloc

--- src/heap.cpp
+++ src/heap.cpp
@@ -1,53 +1,66 @@
 #include <heap.hpp>
 #include <iostream>
 
-Heap::Heap(){
-
+// Constructor
+Heap::Heap()
+{
 }
-//Initializes an empty that is set to store at most N elements
-void Heap::StartHeap(int n){
-    
-    H = (HeapNode*) malloc((N + 1) * sizeof(HeapNode));
+// Initializes an empty that is set to store at most N elements
+void Heap::StartHeap(int n)
+{
+
+    H = (HeapNode *)malloc((N + 1) * sizeof(HeapNode));
+    if (H == NULL){
+        std::cerr << "Error: Out of memory, aborting\n";
+        exit(-1);
+    }
     sizeHeap = 0;
     N = n;
 }
-void Heap::SwapNodes(int parentIndex, int childIndex){
-    
+void Heap::SwapNodes(int parentIndex, int childIndex)
+{
+
     HeapNode temp = H[parentIndex];
     H[parentIndex] = H[childIndex];
     H[childIndex] = temp;
 
 }
-void Heap::HeapifyUp(int index){
-    
-    //Not root node
-    if (index > 1){
+// Moves an element located at the specified index upwards in the heap to correctly reposition it so that the element's prioirty is higher than parents
+void Heap::HeapifyUp(int index)
+{
+    // Not root node
+    if (index > 1)
+    {
         int parent = index / 2;
-        if (H[index].priority < H[parent].priority){
+        if (H[index].priority < H[parent].priority)
+        {
             SwapNodes(parent, index);
-            HeapifyUp(parent);        
+            HeapifyUp(parent);
         }
+        
     }
-
 }
-//Inserts the item with an ordering value (priority) into the heap at its end
-//After inserting uses HeapifyUp to position the item so it maintains the heap order
-//If the heap currently has n elements, the method takes O(logn) time
-void Heap::Insert(int item, int value){
-    
+// Inserts the item with an ordering value (priority) into the heap at its end
+// After inserting uses HeapifyUp to position the item so it maintains the heap order
+// If the heap currently has n elements, the method takes O(logn) time
+void Heap::Insert(int item, int value)
+{
+
     HeapNode node;
     node.data = item;
     node.priority = value;
-    if (sizeHeap < N){
-        H[sizeHeap + 1] = node; //H[0] is kept empty, element is inserted at end of array
+    if (sizeHeap < N)
+    {
+        H[sizeHeap + 1] = node; // H[0] is kept empty, element is inserted at end of array
         sizeHeap += 1;
         HeapifyUp(sizeHeap);
     }
-    
 }
-HeapNode * Heap::getHeap(){
+HeapNode *Heap::getHeap()
+{
     return H;
 }
-void Heap::FreeHeap(){
+void Heap::FreeHeap()
+{
     free(H);
 }
