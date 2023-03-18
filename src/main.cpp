99d99c9f476550a80d81c9fe99434105d6997cdb
msula2 <msula2@uis.edu>
2023-03-19 01:30:09 +0500

    Add Almost Shortest path

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ vector<Edge> *createGraph(int &start, int &end, int &V, int &E)
     vector<Edge> *adj;
 
     FILE *fptr;
-    if ((fptr = fopen("./include/tests/test-49.txt", "r")) == NULL)
+    if ((fptr = fopen("./include/tests/test-46.txt", "r")) == NULL)
     {
         printf("Error in opening file\n");
         exit(1);
@@ -137,7 +137,7 @@ void Dijkstra(vector<Edge> *adj,
         }
     }
 }
-void almostShortestPath(vector<Edge> *adj, vector<int> *prev, int start, int end, int V)
+void bfsDelete(vector<Edge> *adj, vector<int> *prev, int start, int end, int V)
 {
     bool discovered[V] = {false};
     bool deleted[V] = {false};
@@ -164,31 +164,6 @@ void almostShortestPath(vector<Edge> *adj, vector<int> *prev, int start, int end
         queue.erase(queue.begin());
     }
 }
-/*
-while (queue.size() > 0)
-    {
-        u = queue.front();
-        cout << "Front Queue: " << u << "\n";
-        for (auto &edge : prev[u])
-        {
-            index = 0;
-            for (auto &v : adj[edge])
-            {
-                if (v.vertex == u)
-                {
-                    adj[edge].erase(adj[edge].begin() + index);
-                    cout << "Erased edge: "
-                         << "(" << edge << "," << v.vertex << ")"
-                         << "\n";
-                }
-                index += 1;
-            }
-            queue.push_back(edge);
-        }
-        queue.erase(queue.begin());
-    }
-
-*/
 int main()
 {
     int V = 0, E = 0, start = 0, end = 0;
@@ -197,7 +172,9 @@ int main()
     int *dist = new int(V);
     vector<int> *prev = new vector<int>[V];
     Dijkstra(adj, dist, prev, start, V);
-    // printShortestPath(start, V, dist, prev);
-    almostShortestPath(adj, prev, start, end, V);
-    printGraph(adj, V);
+    printShortestPath(start, V, dist, prev);
+    bfsDelete(adj, prev, start, end, V);
+    Dijkstra(adj, dist, prev, start, V);
+    printShortestPath(start, V, dist, prev);
+    
 }
