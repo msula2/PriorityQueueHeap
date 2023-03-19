1ba780c470cbd1ca23bd5917b4f19663df13a581
msula2 <msula2@uis.edu>
2023-03-19 17:03:35 +0500

    Remove dynamic adj list

--- src/main.cpp
+++ src/main.cpp
@@ -1,180 +1,85 @@
 #include <iostream>
-#include <stdio.h>
 #include <vector>
+#include <queue>
 #include <heap.hpp>
 
-#define INFINITY 1000000
 using namespace std;
 
-struct Edge
-{
+const int INF = 1e9; // infinity
 
-    int vertex;
-    int distance;
-};
-vector<Edge> *createGraph(int &start, int &end, int &V, int &E)
+void Dijkstra(int start, vector<pair<int, int>> adj[], int V, int * dist)
 {
-    int lines = 0;
-    vector<Edge> *adj;
+    // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    // pq.push(make_pair(0, start)); // push the starting vertex with distance 0
+    Heap pq{};
+    pq.StartHeap(V);
+    dist[start] = 0;
+    for (int i = 0; i < V; i++)
+    {
+        if (i != start)
+        {
+            dist[i] = INF;
+        }
+        HeapNode node;
+        node.data = i;
+        node.priority = dist[i];
+        pq.Insert(node, dist[i]);
+    }
+    while (pq.getHeapSize() > 0)
+    {
+        // int u = pq.top().second;
+        // pq.pop();
+        HeapNode min = pq.FindMin();
+        pq.ExtractMin();
+        int u = min.data;
+        for (auto v : adj[u])
+        {
+            int neighbor = v.first;
+            int weight = v.second;
 
+            if (dist[neighbor] > dist[u] + weight)
+            {
+                dist[neighbor] = dist[u] + weight;
+                // pq.push(make_pair(dist[neighbor], neighbor));
+                HeapNode n;
+                n.data = neighbor;
+                n.priority = dist[neighbor];
+                pq.ChangeKey(n, dist[neighbor]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int V = 0, E = 0, start = 0, end = 0;
     FILE *fptr;
-    if ((fptr = fopen("./include/tests/test-46.txt", "r")) == NULL)
+    if ((fptr = fopen("./include/tests/test-18.txt", "r")) == NULL)
     {
         printf("Error in opening file\n");
         exit(1);
     }
-    else
+    int v1 = 0, v2 = 0, distance = 0, z1 = 0, z2 = 0;
+    fscanf(fptr, "%d %d\n", &V, &E);
+    fscanf(fptr, "%d %d\n", &start, &end);
+    vector<pair<int, int>> adj[V];
+    for (int i = 0; i < E; i++)
     {
-        int v1 = 0, v2 = 0, distance = 0;
-
-        while (!feof(fptr))
+        fscanf(fptr, "%d %d %d\n", &v1, &v2, &distance);
+        if (v1 != v2)
         {
-            if (lines == 0)
-            {
-                fscanf(fptr, "%d %d\n", &V, &E);
-                lines += 1;
-                adj = new vector<Edge>[V];
-            }
-            else if (lines == 1)
-            {
-                fscanf(fptr, "%d %d\n", &start, &end);
-                lines += 1;
-            }
-            else
-            {
-                fscanf(fptr, "%d %d %d\n", &v1, &v2, &distance);
-                lines += 1;
-                if (v1 != v2)
-                {
-                    Edge e;
-                    e.vertex = v2;
-                    e.distance = distance;
-                    adj[v1].push_back(e);
-                }
-            }
+            adj[v1].push_back(make_pair(v2, distance));
         }
-        fclose(fptr);
+    }
+    fscanf(fptr, "%d %d\n", &z1, &z2);
+    fclose(fptr);
+    int * dist = new int[V];
+    Dijkstra(start, adj, V, dist);
+
+    for (int i = 0; i < V; i++)
+    {
+        cout << "Shortest distance from " << start << " to " << i << " is " << dist[i] << endl;
     }
 
-    return adj;
-}
-void printGraph(vector<Edge> *adj, int V)
-{
-    cout << "\nGraph Contents... \n";
-    int v = 0;
-    for (int x = 0; x < V; x++)
-    {
-        cout << "\n----------------------\n";
-        cout << "Vertex: " << x << "\n";
-        cout << "Neighbors: ";
-        for (int y = 0; y < adj[x].size(); y++)
-        {
-            cout << adj[x][y].vertex
-                 << "(" << adj[x][y].distance
-                 << ")"
-                 << ",";
-        }
-        cout << "\n----------------------\n";
-    }
-}
-void printShortestPath(int start, int V, int *dist, vector<int> *prev)
-{
-    cout << "\nShortest Distance from " << start << " to: \n";
-    for (int t = 0; t < V; t++)
-    {
-        if (t != start)
-        {
-            cout << "Node " << t << " = " << dist[t] << " | Previous = ";
-            for (auto &previous : prev[t])
-            {
-                cout << previous << ",";
-            }
-            cout << "\n";
-        }
-    }
-}
-void Dijkstra(vector<Edge> *adj,
-              int *dist, vector<int> *prev, int start, int V)
-{
-    cout << "\nDijkstra... \n";
-    dist[start] = 0;
-    Heap Q{};
-    Q.StartHeap(V);
-    for (int v = 0; v < V; v++)
-    {
-        if (v != start)
-        {
-            dist[v] = INFINITY;
-        }
-        HeapNode node;
-        node.data = v;
-        node.priority = dist[v];
-        Q.Insert(node, dist[v]);
-    }
-
-    while (Q.getHeapSize() > 0)
-    {
-        HeapNode u = Q.FindMin();
-        Q.ExtractMin();
-        for (auto &neigh : adj[u.data])
-        {
-            int alt = dist[u.data] + neigh.distance;
-            if (alt < dist[neigh.vertex])
-            {
-                dist[neigh.vertex] = alt;
-                prev[neigh.vertex].clear();
-                prev[neigh.vertex].push_back(u.data);
-                HeapNode n;
-                n.data = neigh.vertex;
-                n.priority = alt;
-                Q.ChangeKey(n, alt);
-            }
-            else if (alt == dist[neigh.vertex])
-            {
-                dist[neigh.vertex] = alt;
-                prev[neigh.vertex].push_back(u.data);
-            }
-        }
-    }
-}
-void bfsDelete(vector<Edge> *adj, vector<int> *prev, int start, int end, int V)
-{
-    bool discovered[V] = {false};
-    bool deleted[V] = {false};
-    discovered[end] = true;
-    vector<int> queue;
-    int u = 0, index = 0;
-    queue.push_back(end);
-    while (queue.size() > 0)
-    {
-        u = queue.front();
-        for (auto &edge : prev[u])
-        {
-            index = 0;
-            for (auto &v : adj[edge])
-            {
-                if (v.vertex == u)
-                {
-                    adj[edge].erase(adj[edge].begin() + index);
-                }
-                index += 1;
-            }
-            queue.push_back(edge);
-        }
-        queue.erase(queue.begin());
-    }
-}
-int main()
-{
-    int V = 0, E = 0, start = 0, end = 0;
-    vector<Edge> *adj = createGraph(start, end, V, E);
-    // printGraph(adj);
-    int *dist = new int(V);
-    vector<int> *prev = new vector<int>[V];
-    Dijkstra(adj, dist, prev, start, V);
-    printShortestPath(start, V, dist, prev);
-    bfsDelete(adj, prev, start, end, V);
-    Dijkstra(adj, dist, prev, start, V);
-    printShortestPath(start, V, dist, prev);
-    
+    return 0;
 }
